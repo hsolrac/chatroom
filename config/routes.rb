05d0cde39d77747dc113ff566268f3619229f706
hsolrac <ch.henriquevinha@gmail.com>
2024-03-18 18:35:47 -0300

    wip: home

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
   get  "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
   get  "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  
+  root "home#index"
+
   resources :sessions, only: [:index, :show, :destroy]
   resource  :password, only: [:edit, :update]
+  resources  :rooms
+  get 'users/:id', to: 'users#show', as: :user
   namespace :identity do
     resource :email,              only: [:edit, :update]
     resource :email_verification, only: [:show, :create]
     resource :password_reset,     only: [:new, :edit, :create, :update]
   end
-  root "home#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_15_202639) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_18_192020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_private", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
